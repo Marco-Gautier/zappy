3d63ffa6d71685e93ea876dba63b67d89f5b7947
Pacôme Françon <pacome.francon@epitech.eu>
2019-05-15 10:33:47 +0200

    test(): add unit test on parse_options

---
+++ server/tests/src/tests_parse_options.c
@@ -0,0 +1,114 @@
+/*
+** EPITECH PROJECT, 2019
+** tests_parse_options
+** File description:
+** tests_parse_options
+*/
+
+#include <assert.h>
+#include <criterion/criterion.h>
+#include <unistd.h>
+#include "zappy.h"
+#include "my.h"
+
+static void init()
+{
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
+
+Test(parse_options, _default, .init=init)
+{
+    struct server_opt options;
+    static const char * const argv[] = {
+        "./zappy_server",
+        NULL
+    };
+    int argc = ARRAY_SIZE(argv) - 1;
+    char **team_names;
+
+    assert(parse_options(&options, argc, (char **)argv) != -1);
+    assert(options.port == default_options.port);
+    assert(options.freq == default_options.freq);
+    assert(options.width == default_options.width);
+    assert(options.height == default_options.height);
+    assert(options.max_team_nbr == default_options.max_team_nbr);
+    assert(options.team_names != NULL);
+    team_names = my_str_to_word_array(strdup(default_team_names), " ");
+    for (size_t i = 0; options.team_names[i] != NULL; i++)
+        assert(!strcmp(team_names[i], options.team_names[i]));
+}
+
+Test(parse_options, all, .init=init)
+{
+    struct server_opt options;
+    static const char * const argv[] = {
+        "./zappy_server",
+        "-p", "8080",
+        "--frequence", "42",
+        "-x", "21",
+        "--height", "84",
+        "-c", "150",
+        "--team-names", "mdr1", "mdr2",
+        NULL
+    };
+    int argc = ARRAY_SIZE(argv) - 1;
+
+    assert(parse_options(&options, argc, (char **)argv) != -1);
+    assert(options.port == 8080);
+    assert(options.freq == 42);
+    assert(options.width == 21);
+    assert(options.height == 84);
+    assert(options.max_team_nbr == 150);
+    assert(options.team_names != NULL);
+    assert(!strcmp(options.team_names[0], "mdr1"));
+    assert(!strcmp(options.team_names[1], "mdr2"));
+}
+
+Test(parse_options, bad_short_options, .init=init)
+{
+    struct server_opt options;
+    static const char * const argv[] = {
+        "./zappy_server",
+        "-p", "mdr",
+        NULL
+    };
+    int argc = ARRAY_SIZE(argv) - 1;
+    char **team_names;
+
+    cr_assert(parse_options(&options, argc, (char **)argv) == -1);
+    cr_assert(options.port == 0);
+    cr_assert(options.freq == default_options.freq);
+    cr_assert(options.width == default_options.width);
+    cr_assert(options.height == default_options.height);
+    cr_assert(options.max_team_nbr == default_options.max_team_nbr);
+    cr_assert(options.team_names != NULL);
+    team_names = my_str_to_word_array(strdup(default_team_names), " ");
+    for (size_t i = 0; options.team_names[i] != NULL; i++)
+        assert(!strcmp(team_names[i], options.team_names[i]));
+}
+
+Test(parse_options, bad_options, .init=init)
+{
+    struct server_opt options;
+    static const char * const argv[] = {
+        "./zappy_server",
+        "-c", "150",
+        "--team-names",
+        "-p", "8080",
+        NULL
+    };
+    int argc = ARRAY_SIZE(argv) - 1;
+    char **team_names;
+
+    cr_assert(parse_options(&options, argc, (char **)argv) != -1);
+    cr_assert(options.port == default_options.port);
+    cr_assert(options.freq == default_options.freq);
+    cr_assert(options.width == default_options.width);
+    cr_assert(options.height == default_options.height);
+    cr_assert(options.max_team_nbr == 150);
+    cr_assert(options.team_names != NULL);
+    team_names = my_str_to_word_array(strdup(default_team_names), " ");
+    for (size_t i = 0; options.team_names[i] != NULL; i++)
+        assert(!strcmp(team_names[i], options.team_names[i]));
+}
