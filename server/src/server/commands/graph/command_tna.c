dfc37541f1dfa7236b72b2b3da094bf4029580af
Pacôme Françon <pacome.francon@epitech.eu>
2019-05-23 19:16:21 +0200

    fix(server): send tna response in an call

--- server/src/server/commands/graph/command_tna.c
+++ server/src/server/commands/graph/command_tna.c
@@ -6,15 +6,24 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "zappy.h"
 
 static const char *format = "tna %s\n";
 
 int command_tna(struct server *server, int i, int argc, char **argv)
 {
+    char buffer[BUFSIZ];
+    char tmp[128];
+
     (void)argc;
     (void)argv;
-    for (size_t j = 0; server->options.team_names[j] != NULL; j++)
-        dprintf(server->clients[i]->fd, format, server->options.team_names[j]);
+    buffer[0] = '\0';
+    for (size_t j = 0; server->options.team_names[j] != NULL; j++) {
+        snprintf(tmp, sizeof(tmp), format, server->options.team_names[j]);
+        strcat(buffer, tmp);
+    }
+    if (buffer[0] != '\0')
+        dprintf(server->clients[i]->fd, "%s", buffer);
     return 0;
 }
