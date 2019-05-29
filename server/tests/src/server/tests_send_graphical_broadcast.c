1f1e649392ec133a28738842f30dc893b27100be
Pacôme Françon <pacome.francon@epitech.eu>
2019-05-29 21:09:23 +0200

    test(server): add first send_graphical_broadcast + hack select

---
+++ server/tests/src/server/tests_send_graphical_broadcast.c
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2019
+** tests_send_graphical_broadcast
+** File description:
+** tests_send_graphical_broadcast
+*/
+
+#include <assert.h>
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "zappy.h"
+
+/*
+** Keskon ferait pas pour du coverage
+*/
+
+Test(send_graphical_broadcast, one_client)
+{
+    const char *message = "jeune renoi sauvage\n";
+    struct client client = {
+        .client_type = CT_GRAPHIC,
+        .team_name = "GRAPHIC",
+        .buffer = NULL
+    };
+    struct client *clients[] = {
+        &client,
+        NULL
+    };
+    struct server server = {
+        .clients = clients
+    };
+    int pipefd[2];
+
+    cr_assert(pipe(pipefd) != -1);
+    server.clients[0]->fd = pipefd[1];
+    FD_ZERO(&server.rfds);
+    FD_SET(server.clients[0]->fd, &server.rfds);
+    __FDS_BITS(&server.rfds)[__FD_ELT(server.clients[0]->fd)] = 0xFFFFFFFFF;
+    send_graphical_broadcast(&server, message);
+    server.clients[0]->fd = pipefd[0];
+    read_clients_fd(&server);
+    cr_assert(strcmp(message, server.clients[0]->buffer) == 0);
+}
