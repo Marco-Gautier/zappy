28ff0663828a47d766759c4f950d34600b8ed7c5
Pacôme Françon <pacome.francon@epitech.eu>
2019-05-30 21:02:20 +0200

    test(server): add 1 send_graphical_broadcast test

--- server/tests/src/server/tests_send_graphical_broadcast.c
+++ server/tests/src/server/tests_send_graphical_broadcast.c
@@ -42,3 +42,31 @@ Test(send_graphical_broadcast, one_client)
     read_clients_fd(&server);
     cr_assert(strcmp(message, server.clients[0]->buffer) == 0);
 }
+
+Test(send_graphical_broadcast, one_client_2)
+{
+    const char *message = "jeune renoi \nsauvage\n";
+    struct client client = {
+        .client_type = CT_GRAPHIC,
+        .team_name = "GRAPHIC",
+        .buffer = strdup("")
+    };
+    struct client *clients[] = {
+        &client,
+        NULL
+    };
+    struct server server = {
+        .clients = clients
+    };
+    int pipefd[2];
+
+    cr_assert(pipe(pipefd) != -1);
+    server.clients[0]->fd = pipefd[1];
+    FD_ZERO(&server.rfds);
+    FD_SET(server.clients[0]->fd, &server.rfds);
+    __FDS_BITS(&server.rfds)[__FD_ELT(server.clients[0]->fd)] = 0xFFFFFFFFF;
+    send_graphical_broadcast(&server, message);
+    server.clients[0]->fd = pipefd[0];
+    read_clients_fd(&server);
+    cr_assert(strcmp(message, server.clients[0]->buffer) == 0);
+}
