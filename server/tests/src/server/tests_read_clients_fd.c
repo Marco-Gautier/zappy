c9d02d5017d8f8d3bf0688e4361c4312ac9f4436
Pacôme Françon <pacome.francon@epitech.eu>
2019-05-30 20:43:13 +0200

    test(server): add read_clients_fd tests

---
+++ server/tests/src/server/tests_read_clients_fd.c
@@ -0,0 +1,64 @@
+/*
+** EPITECH PROJECT, 2019
+** tests_read_clients_fd
+** File description:
+** tests_read_clients_fd
+*/
+
+#include <assert.h>
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "zappy.h"
+
+Test(read_clients_fd, no_client)
+{
+    struct server server = {
+        .clients = NULL
+    };
+
+    read_clients_fd(&server);
+    assert(server.clients == NULL);
+}
+
+static struct client *create_client(int id)
+{
+    struct client *client = malloc(sizeof(struct client));
+
+    client->client_type = CT_GRAPHIC;
+    client->team_name = "GRAPHIC";
+    client->buffer = NULL;
+    client->id = id;
+    return client;
+}
+
+Test(read_clients_fd, 2_clients)
+{
+    struct client *client1 = create_client(42);
+    struct client *client2 = create_client(84);
+    struct client *clients[] = {
+        client1,
+        client2,
+        NULL
+    };
+    struct server server = {
+        .clients = clients
+    };
+    int pipefd1[2];
+    int pipefd2[2];
+
+    close(STDOUT_FILENO);
+    cr_assert(pipe(pipefd1) != -1);
+    cr_assert(pipe(pipefd2) != -1);
+    server.clients[0]->fd = pipefd1[1];
+    server.clients[1]->fd = pipefd2[1];
+    FD_ZERO(&server.rfds);
+    FD_SET(server.clients[0]->fd, &server.rfds);
+    FD_SET(server.clients[1]->fd, &server.rfds);
+    __FDS_BITS(&server.rfds)[__FD_ELT(server.clients[0]->fd)] = 0;
+    __FDS_BITS(&server.rfds)[__FD_ELT(server.clients[1]->fd)] = 0xFFFFFFFFF;
+    server.clients[1]->fd = pipefd1[0];
+    read_clients_fd(&server);
+    read_clients_fd(&server);
+    cr_assert(strcmp("pdi 42\ndead\n", server.clients[0]->buffer) == 0);
+}
