bac76a6194227c729f7239467dd04ece303d76e0
Pacôme Françon <pacome.francon@epitech.eu>
2019-06-05 19:21:52 +0200

    feat(front): add clients in color on the map

--- front/client.py
+++ front/client.py
@@ -1,6 +1,20 @@
 import re
+import random
 
 from draw import draw_text
+import pygame
+
+def random_color():
+    return pygame.Color(random.randint(0, 0xFF), random.randint(0, 0xFF), random.randint(0, 0xFF))
+
+def hex_to_rgb(hex):
+    hex = str(hex)
+    hex = hex.lstrip('#')
+    hlen = len(hex)
+    print(hex)
+    tmp = tuple(int(hex[i:i+int(hlen/3)], 16) for i in range(0, hlen, int(hlen/3)))
+    print(tmp)
+    return tmp
 
 """ Represent a AI client """
 class Client:
@@ -8,12 +22,14 @@ class Client:
     """ Constructor """
     def __init__(self, input):
         self.id = int(re.search(r"^#([1-9]+\d*)$", input[1]).groups()[0])
-        self.pos_x = int(input[2])
-        self.pos_y = int(input[3])
+        self.x = int(input[2])
+        self.y = int(input[3])
         self.direction = int(input[4])
         self.level = int(input[5])
         self.team_name = input[6]
+        self.color = random_color()
+        # print(pygame.Color.normalize(self.color))
 
     def draw_interface(self, window, font, y):
-        text = f"#{self.id} {self.pos_x} {self.pos_y} {self.direction} {self.level} {self.team_name}"
-        draw_text(window, font, text, 1700, y)
+        text = f"#{self.id} {self.x} {self.y} {self.direction} {self.level} {self.team_name}"
+        draw_text(window, font, text, 1700, y, self.color)
--- front/draw.py
+++ front/draw.py
@@ -1,7 +1,7 @@
 import pygame
 
 def draw_text(window, font, text, x, y, color = (255, 255, 0)):
-    label = font.render(text, 1, color)
+    label = font.render(text, True, color)
     window.blit(label, (x, y))
 
 def draw_interface(window, font, teams_name, width, height):
@@ -21,6 +21,13 @@ def draw_client_interface(window, font, clients):
     for client in clients:
         client.draw_interface(window, font, y)
         y += 50
+
+def draw_client(window, width, height, clients):
+    TILE_SIZE = (1080 - 20 - 20 - 100) / height
+    for client in clients:
+        x = client.x * TILE_SIZE + 1920 / 2 - width / 2 * TILE_SIZE + TILE_SIZE * 0.2
+        y = client.y * TILE_SIZE + 1080 / 2 - height / 2 * TILE_SIZE + TILE_SIZE * 0.2
+        pygame.draw.rect(window, client.color, pygame.Rect(x, y, TILE_SIZE * 0.6, TILE_SIZE * 0.6))
 
 def draw_map(window, width, height):
     TILE_SIZE = (1080 - 20 - 20 - 100) / height
