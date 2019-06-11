518974fb397a315e53eea4cab77bc031c95f86c8
Pacôme Françon <pacome.francon@epitech.eu>
2019-06-11 11:30:54 +0200

    test(server): add test command_mct

---
+++ server/tests/src/server/commands/graph/tests_command_mct.c
@@ -0,0 +1,55 @@
+/*
+** EPITECH PROJECT, 2019
+** tests_command_mct
+** File description:
+** tests_command_mct
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <criterion/criterion.h>
+#include "zappy.h"
+
+static void setup_expected_result(char expected_result[1024])
+{
+    snprintf(expected_result, 1024,
+             "%s\n%s\n%s\n%s\n%s\n%s\n",
+             "bct 0 0 0 0 0 0 0 0 0",
+             "bct 1 0 0 0 0 0 0 0 0",
+             "bct 0 1 0 0 0 0 0 0 0",
+             "bct 1 1 0 0 0 0 0 0 0",
+             "bct 0 2 0 0 0 0 0 0 0",
+             "bct 1 2 0 0 0 0 0 0 0");
+}
+
+Test(command_mct, success)
+{
+    struct client client;
+    struct client *clients[] = {
+        &client,
+        NULL
+    };
+    int argc = 1;
+    int pipefd[2];
+    struct server server = {
+        .clients = clients,
+        .options = {
+            .width = 2,
+            .height = 3
+        }
+    };
+    static const char * const argv[] = { "mct" };
+    char buffer[1024] = { 0 };
+    char expected_result[1024] = { 0 };
+
+    assert(pipe(pipefd) == 0);
+    client.fd = pipefd[1];
+    setup_expected_result(expected_result);
+    init_world(&server.world, &server.options);
+    assert(command_mct(&server, 0, argc, (char **)argv) == 0);
+    read(pipefd[0], buffer, 1024);
+    assert(strcmp(buffer, expected_result) == 0);
+    close(pipefd[0]);
+    close(pipefd[1]);
+}
