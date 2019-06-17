a5f93fd6116672592f9ecff40edb8a96203da920
Marco GAUTIER <marco.gautier@epitech.eu>
2019-06-17 14:38:48 +0200

    fix(travis): I'm going to die

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,5 @@ git config --global push.default simple # we only want to push one branch — ma
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
 git remote add production ssh://marco@51.77.202.182:4242/home/marco/apps/zappy-deploy
-git push production master # push our updates
+git push --force production master # push our updates
 ssh marco@51.77.202.182 -p 4242 "sh -c 'cd apps/zappy-deploy; nohup docker-compose up --build > /dev/null 2>&1 &'"
