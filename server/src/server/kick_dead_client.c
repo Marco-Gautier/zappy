a3a915c5cc61a80c72a95f60dd01c18dfac051b9
Pacôme Françon <pacome.francon@epitech.eu>
2019-06-18 11:48:07 +0200

    fix(server): fix kick_dead_client loop

--- server/src/server/kick_dead_client.c
+++ server/src/server/kick_dead_client.c
@@ -16,7 +16,9 @@ void kick_dead_client(struct server *server)
     for (size_t i = 0; server->clients[i] != NULL; i++) {
         if (server->clients[i]->client_type == CT_GRAPHIC)
             continue;
-        if (server->clients[i]->inventory.food < 0)
+        if (server->clients[i]->inventory.food < 0) {
             kick_client_from_server(server, i);
+            i = 0;
+        }
     }
 }
