a1b9efee905afd36117f76a39479e02c800b02b1
Marco GAUTIER <marco.gautier@epitech.eu>
2019-06-19 17:03:17 +0200

    refactor(server): change graph communication to remove dead code

--- server/src/server/send_graphical_broadcast.c
+++ server/src/server/send_graphical_broadcast.c
@@ -14,15 +14,11 @@ void send_graphical_broadcast(struct server *server, const char *format, ...)
 {
     va_list ap;
 
-    if (!format)
-        return;
     va_start(ap, format);
     for (size_t i = 0; server->clients[i] != NULL; i++) {
         if (!server->clients[i]->team_name)
             continue;
         if (server->clients[i]->client_type != CT_GRAPHIC)
-            continue;
-        if (strcmp(server->clients[i]->team_name, "GRAPHIC") != 0)
             continue;
         vdprintf(server->clients[i]->fd, format, ap);
     }
