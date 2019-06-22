50d24510561cb14ac8b5f715db1cdac2ed02a90b
Marco GAUTIER <marco.gautier@epitech.eu>
2019-06-22 23:05:43 +0200

    test(server): add test command_broadcast

---
+++ server/tests/src/server/commands/ai/tests_command_broadcast.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_zappy_2018
+** File description:
+** tests_command_broadcast
+*/
+
+#include <criterion/criterion.h>
+#include "zappy.h"
+
+int broadcast_callback(struct server *s, struct client *client, int ac, char **av);
+
+Test(command_broadcast, test_forward_event_creation)
+{
+    struct client client = { .event = NULL };
+    struct client *clients[] = {
+        &client,
+        NULL
+    };
+    struct server server = {
+        .clients = clients,
+        .options.freq = 100
+    };
+
+    cr_assert(command_broadcast(&server, 0, 1, NULL) == 0);
+    cr_assert(server.clients[0]->event != NULL);
+    cr_assert(server.clients[0]->event->callback == broadcast_callback);
+}
